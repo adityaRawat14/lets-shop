c010cd6d67224322ab974fdf8b044440cb50ff90
Aditya Rawat <125037123+adityawillcode@users.noreply.github.com>
2024-03-18 01:10:30 +0545

    admin auth

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,9 @@ export async function middleware(request: NextRequest) {
   
   const cookies=request.cookies.get('next-auth.session-token')
   
-  
+  if(request.nextUrl.pathname=='/shop/cart' && !cookies){
+    return NextResponse.redirect(new URL('/shop/dashboard',request.url))
+  }
 
   if((request.nextUrl.pathname=='/auth/login' || request.nextUrl.pathname=='/auth/signup') && cookies){
     return NextResponse.redirect(new URL('/shop/dashboard',request.url))
